9389d743f94203a7dcdc3e81819c1d227ebc1a87
Nick Perry <nick@theperry.co>
2019-11-08 10:00:20 -0500

    Added update and detail methods

--- app.py
+++ app.py
@@ -16,7 +16,19 @@ def createWisdom():
     wisdomToSave.save()
     wisdomToSave.isUpvoted = True
     wisdomToSave.save()
-    return str(len(Wisdom.objects(id = wisdomToSave.id)))
-
+    return str(Wisdom.objects(id = wisdomToSave.id).first().id)
+@app.route('/wisdom/details', methods=['GET'])
+def getSingleWisdom():
+    id = request.args.get('id', type = str)
+    return Wisdom.objects(id = id).first().content
+@app.route('/wisdom/update', methods=['PUT'])
+def updateWisdom():
+    wisdomUpdateModel = request.get_json()
+    wisdomToFindId = wisdomUpdateModel['id']
+    Wisdom.objects(id = wisdomToFindId).update(
+        content = wisdomUpdateModel['content'],
+        author = wisdomUpdateModel['author']
+    )
+    return  Wisdom.objects(id = wisdomToFindId).first().content
 if __name__ == "__main__":
     app.run(debug=True,port=8080)
